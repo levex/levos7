da37459f16bb2abcddbb7866232626e123e5888c
Levente Kurusa <levex@linux.com>
2017-08-19 04:09:57 -0500

    Travis try #3
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- buildtools/build-cross-compiler.sh
+++ buildtools/build-cross-compiler.sh
@@ -6,37 +6,37 @@ gcc_ver=gcc-6.2.0
 set -e
 
 ######## Prepare
-# mkdir cross-compiler/
-# mkdir cross-compiler/src
-# mkdir cross-compiler/build
-# mkdir cross-compiler/prefix
-# 
- cross_compiler=`pwd`/cross-compiler
- _ncores=`./determine_cores.sh`
-# 
-# ######## Binutils
-# 
-# echo Fetching binutils sources...
-# [[ ! -e ${cross_compiler}/src/${binutils_ver}.tar.gz ]] && \
-# 	wget https://ftp.gnu.org/gnu/binutils/${binutils_ver}.tar.gz \
-# 		-O ${cross_compiler}/src/${binutils_ver}.tar.gz
-# cd ${cross_compiler}/src
-# 
-# tar xzvf ${binutils_ver}.tar.gz
-# 
-# echo Building binutils
-# cd ${cross_compiler}/build
-# mkdir build-binutils
-# cd build-binutils
-# ../../src/${binutils_ver}/configure \
-# 	--prefix=${cross_compiler}/prefix \
-# 	--target=i686-elf \
-# 	--with-sysroot \
-# 	--disable-nls \
-# 	--disable-werror
-# 
-# make -j${_ncores}
-# make install 
+mkdir cross-compiler/
+mkdir cross-compiler/src
+mkdir cross-compiler/build
+mkdir cross-compiler/prefix
+
+cross_compiler=`pwd`/cross-compiler
+_ncores=`./determine_cores.sh`
+
+######## Binutils
+
+echo Fetching binutils sources...
+[[ ! -e ${cross_compiler}/src/${binutils_ver}.tar.gz ]] && \
+	wget https://ftp.gnu.org/gnu/binutils/${binutils_ver}.tar.gz \
+		-O ${cross_compiler}/src/${binutils_ver}.tar.gz
+cd ${cross_compiler}/src
+
+tar xzvf ${binutils_ver}.tar.gz
+
+echo Building binutils
+cd ${cross_compiler}/build
+mkdir build-binutils
+cd build-binutils
+../../src/${binutils_ver}/configure \
+	--prefix=${cross_compiler}/prefix \
+	--target=i686-elf \
+	--with-sysroot \
+	--disable-nls \
+	--disable-werror
+
+make -j${_ncores}
+make install 
 
 ######## GCC
 
